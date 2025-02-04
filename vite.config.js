a59a1529848720e7bc8d99696e49516baf53dc27
JoaquinMulet <joaquin.mulet@gmail.com>
2025-02-03 23:38:39 -0300

    UI improvements: Updated favicon with Indekura 'I', centered home page, removed hover effects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'favicon.png') {
+            return 'favicon.png';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 })
